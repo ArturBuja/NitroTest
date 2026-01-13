3727530f79966c4828f867ff7db0c105b7416cb0
Artur Buja <artur.buja2@gmail.com>
2026-01-13 08:27:03 +0100

    test

--- myModule/src/index.ts
+++ myModule/src/index.ts
@@ -1,1 +1,8 @@
 // TODO: Export all HybridObjects here for the user
+import { NitroModules } from 'react-native-nitro-modules'
+import { type Math } from './specs/Example.nitro'
+const NitroHybridObject = NitroModules.createHybridObject<Math>('Math')
+
+export function add(a: number, b: number): number {
+  return NitroHybridObject.add(a, b)
+}
--- myModule/src/specs/Example.nitro.ts
+++ myModule/src/specs/Example.nitro.ts
@@ -1,1 +1,10 @@
 // TODO: Export specs that extend HybridObject<...> here
+import { type HybridObject } from 'react-native-nitro-modules'
+
+export interface Math
+  extends HybridObject<{
+    ios: 'swift'
+    android: 'kotlin'
+  }> {
+  add(a: number, b: number): number
+}
